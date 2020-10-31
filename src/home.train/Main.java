f8a9768565dc1fee310355e4e083e458b8c4c9a8
hossein nick <hossinnick62@gmail.com>
2020-10-31 15:36:17 +0330

    add another foreach with different if conditional

--- src/home.train/Main.java
+++ src/home.train/Main.java
@@ -29,5 +29,12 @@ public class Main {
                 System.out.println(employee.getAge());
             }
         });
+
+        employees.forEach(employee -> {
+            if(employee.getAge()<30){
+                System.out.println(employee.getName());
+                System.out.println(employee.getAge());
+            }
+        });
     }
 }
