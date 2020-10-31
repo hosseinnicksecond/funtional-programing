1e01d54f794527531e6c4839bba66408ca737d1f
hossein nick <hossinnick62@gmail.com>
2020-10-31 15:31:50 +0330

    add Employee class and forEach with consumer

---
+++ src/home.train/Main.java
@@ -0,0 +1,26 @@
+package home.train;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee hossein = new Employee("hossein fozol", 36);
+        Employee hessam = new Employee("hessam khob", 30);
+        Employee farhad = new Employee("farhad kachal", 21);
+        Employee nooosh = new Employee("noosh topol", 24);
+        Employee javad = new Employee("javad aziz", 32);
+        Employee gazal = new Employee("gazal abji", 30);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(hessam);
+        employees.add(hossein);
+        employees.add(nooosh);
+        employees.add(farhad);
+        employees.add(javad);
+        employees.add(gazal);
+
+        employees.forEach(employee -> System.out.println(employee.getName()));
+    }
+}
