2f3b269ddcac1e8e146515494ee8f01d36a320b5
hossein nick <hossinnick62@gmail.com>
2020-11-01 20:51:08 +0330

    using collect method and flatMap method

--- src/home.train/Main.java
+++ src/home.train/Main.java
@@ -3,6 +3,8 @@ package home.train;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -30,10 +32,42 @@ public class Main {
                 new Employee("mario lopez", 30), new Employee("lissa parrot", 40),
                 new Employee("alex farm", 35), new Employee("sara jones", 27));
 
-        System.out.println(Stream.concat(employeeStream1,employeeStream2)
-                .distinct()
-                .peek(System.out::println)
-                .count());
+//        System.out.println(Stream.concat(employeeStream1, employeeStream2)
+//                .distinct()
+//                .peek(System.out::println)
+//                .count());
+
+        System.out.println("######################");
+
+        Department hr = new Department("Human Resource");
+        Department account = new Department("Accounting");
+
+       employeeStream1.forEach(hr::addEmployee);
+       employeeStream2.forEach(account::addEmployee);
+
+
+
+        List<Department> company = new ArrayList<>();
+        company.add(hr);
+        company.add(account);
+
+        company.stream()
+                .flatMap(department -> department.getEmployeeList().stream())
+                .forEach(System.out::println);
+
+        new Thread(() -> {
+            Set<String> firstName=company.stream()
+                    .flatMap(department -> department.getEmployeeList().stream())
+                    .map(s->s.getName().substring(0,s.getName().indexOf(" ")))
+                    .map(String::toUpperCase)
+                    .distinct()
+                    .peek(System.out::println)
+                    .collect(Collectors.toSet());
+
+            firstName.forEach(System.out::println);
+
+        }).start();
+
 
     }
 }
