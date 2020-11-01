2dc3013b70e65f3b6fc78060174ce66bd4026471
hossein nick <hossinnick62@gmail.com>
2020-11-01 21:14:33 +0330

    using different type of collect method and create a map

--- src/home.train/Main.java
+++ src/home.train/Main.java
@@ -1,9 +1,6 @@
 package home.train;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,14 +20,25 @@ public class Main {
                 .forEach(System.out::println);
         System.out.println("******==========********");
 
-
-        Stream<Employee> employeeStream1 = Stream.of(new Employee("john Doe", 22),
+        List<Employee> employees1=Arrays.asList(
+                new Employee("john Doe", 22),
                 new Employee("mary public", 36), new Employee("john hill", 33),
-                new Employee("mario lopez", 30), new Employee("sara jones", 27));
-
-        Stream<Employee> employeeStream2 = Stream.of(new Employee("toni liz", 21),
+                new Employee("mario lopez", 30), new Employee("sara jones", 28)
+        );
+        List<Employee> employees2 = Arrays.asList(new Employee("toni liz", 21),
                 new Employee("mario lopez", 30), new Employee("lissa parrot", 40),
                 new Employee("alex farm", 35), new Employee("sara jones", 27));
+
+
+
+//        Stream<Employee> employeeStream1
+//                = Stream.of(new Employee("john Doe", 22),
+//                new Employee("mary public", 36), new Employee("john hill", 33),
+//                new Employee("mario lopez", 30), new Employee("sara jones", 27));
+//
+//        Stream<Employee> employeeStream2 = Stream.of(new Employee("toni liz", 21),
+//                new Employee("mario lopez", 30), new Employee("lissa parrot", 40),
+//                new Employee("alex farm", 35), new Employee("sara jones", 27));
 
 //        System.out.println(Stream.concat(employeeStream1, employeeStream2)
 //                .distinct()
@@ -42,31 +50,48 @@ public class Main {
         Department hr = new Department("Human Resource");
         Department account = new Department("Accounting");
 
-       employeeStream1.forEach(hr::addEmployee);
-       employeeStream2.forEach(account::addEmployee);
-
-
-
+       employees1.forEach(hr::addEmployee);
+       employees2.forEach(account::addEmployee);
+//
+//
+//
         List<Department> company = new ArrayList<>();
         company.add(hr);
         company.add(account);
+//
+//        company.stream()
+//                .flatMap(department -> department.getEmployeeList().stream())
+//                .forEach(System.out::println);
+//
+//        new Thread(() -> {
+//            Set<String> firstName=company.stream()
+//                    .flatMap(department -> department.getEmployeeList().stream())
+//                    .map(s->s.getName().substring(0,s.getName().indexOf(" ")))
+//                    .map(String::toUpperCase)
+//                    .distinct()
+//                    .peek(System.out::println)
+//                    .collect(Collectors.toSet());
+//
+//            firstName.forEach(System.out::println);
+//
+//        }).start();
 
-        company.stream()
+        List<String> sortedGNumber=someBingo
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s->s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add,ArrayList::addAll);
+
+        sortedGNumber.forEach(System.out::println);
+
+        Map<Integer, List<Employee>> mappedByAge=company.stream()
                 .flatMap(department -> department.getEmployeeList().stream())
-                .forEach(System.out::println);
+                .collect(Collectors.groupingBy(e->e.getAge()));
 
-        new Thread(() -> {
-            Set<String> firstName=company.stream()
-                    .flatMap(department -> department.getEmployeeList().stream())
-                    .map(s->s.getName().substring(0,s.getName().indexOf(" ")))
-                    .map(String::toUpperCase)
-                    .distinct()
-                    .peek(System.out::println)
-                    .collect(Collectors.toSet());
+//        mappedByAge.values().forEach(System.out::println);
 
-            firstName.forEach(System.out::println);
 
-        }).start();
 
 
     }
